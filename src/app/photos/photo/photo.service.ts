13b4b7718292ba11605ee84767a4638efcbc87a8
blericalopes <blericalopes@outlook.com>
2023-02-13 22:49:17 -0300

    httpParams adicionado para exibir apenas 12 fotos equivalentes a pagina 1 como definido no back end

--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -1,15 +1,24 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 import { Photo } from './photo';
 
 const API = 'http://localhost:3000';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class PhotoService {
-    constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient) { }
+
     listFromUser(userName: string) {
         return this.http
             .get<Photo[]>(API + '/flavio/photos');
+    }
+
+    listFromUserPaginated(userName: string, page: number) {
+        const params = new HttpParams()
+            .append('page', page.toString());
+
+        return this.http
+            .get<Photo[]>(API + '/flavio/photos', { params });
     }
 }
