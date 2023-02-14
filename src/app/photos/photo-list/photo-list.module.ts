1e772779522a69496845d57375674da1223973c4
blericalopes <blericalopes@outlook.com>
2023-02-14 14:42:02 -0300

    componetizei o campo de busca obs: filtro parou de funcionar

--- src/app/photos/photo-list/photo-list.module.ts
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -7,13 +7,15 @@ import { FilterByDescription } from './filter-by-description.pipe';
 import { LoadButtonComponent } from './load-button/load-button.component';
 import { PhotoListComponent } from './photo-list.component';
 import { PhotosComponent } from './photos/photos.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
     declarations: [
         PhotoListComponent,
         PhotosComponent,
         LoadButtonComponent,
-        FilterByDescription
+        FilterByDescription,
+        SearchComponent
     ],
     imports: [
         CommonModule,
---
+++ src/app/photos/photo-list/search/search.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
+
+@Component({
+    selector: 'pp-search',
+    templateUrl: './search.component.html'
+})
+
+export class SearchComponent implements OnInit, OnDestroy {
+    
+    debounce: Subject<string> = new Subject<string>();
+    
+    ngOnInit(): void {
+        this.debounce
+        .pipe(debounceTime(300));
+    }
+    
+    ngOnDestroy(): void {
+        this.debounce.unsubscribe();
+    }
+}
